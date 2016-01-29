6060b9316dc1d87275a48e1dcf7f977473bb3301
Joseph Atallah <j_atall@live.concordia.ca>
2016-01-29 10:47:35 -0500

    Added the whole project. The ibeacon plugin is imported in the project. Able to detect an iBeacon through an iBeacon prefab and an iBeaconReceiver prefab.

---
+++ Assets/Source/iBeacon/Editor/PostProcessBuildTrigger.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.IO;
+using UnityEditor.Callbacks;
+
+public static class PostProcessBuildTrigger {
+#if UNITY_IOS
+	[PostProcessBuild]
+	public static void OnPostProcessBuild(BuildTarget target, string path) {
+		string plistpath = path;
+		updateInfoPlist(plistpath,"NSLocationAlwaysUsageDescription",PlayerPrefs.GetString("NSLocationUsageDescription","DUMMY, bitte in Scene ändern"));
+
+	}
+
+	public static void updateInfoPlist(string path, string key, string value) {
+		string plistfile = path + "/Info.plist";
+		string[] lines = System.IO.File.ReadAllLines(plistfile);
+		string newcontent = "";
+		for (int i = 0; i < lines.Length; i++) {
+			if (i < lines.Length - 2)  {
+				if (!lines[i].Contains(key)) {
+					newcontent += lines[i];
+					newcontent += "\n";
+				}
+			}
+		}
+		newcontent += "\t<key>"+key+"</key>\n<string>"+value+"</string>\n"+lines[lines.Length-2]+"\n"+lines[lines.Length-1];
+		FileStream filestr = new FileStream(plistfile,FileMode.Create);
+		filestr.Close();
+		StreamWriter plistWriter = new StreamWriter(plistfile);
+		plistWriter.Write(newcontent);
+		plistWriter.Close();
+	}
+#endif
+}
