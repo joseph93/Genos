8d1cbdedcd9b995a34f22f0e6dd8975ca8cce375
Joseph Atallah <j_atall@live.concordia.ca>
2016-02-10 03:25:05 -0500

    Search iBeacon scene was crashing when loaded. Bug fixed. It doesn't crash anymore.

--- Assets/Scripts/TourButton.cs
+++ Assets/Scripts/TourButton.cs
@@ -2,20 +2,26 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class TourButton : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-    public void changeSceneToBeacon()
+namespace Assets.Scripts
+{
+    public class TourButton : MonoBehaviour
     {
-        SceneManager.LoadScene("Search iBeacon");
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void changeSceneToBeacon()
+        {
+            SceneManager.LoadScene("Search iBeacon");
+        }
     }
 }
