dfe4a9b0f5e57577d7c96736b8f62cb9e1b8f451
Joseph Atallah <j_atall@live.concordia.ca>
2016-02-12 16:35:15 -0500

    Merged the beacon and the map modules together.

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+    class Node
+    {
+        private int x;
+        private int y;
+
+        public Node(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+
+    }
+}
