02e772894d8f9382ea6089e3b093b0881fb5b554
Joseph Atallah <j_atall@live.concordia.ca>
2016-03-10 17:07:22 -0500

    I have modified the UI of the windows that pop up with the observer pattern. Added OP-21: A pop up is displayed when close to an IBeacon. The PopUpWindow.cs has been added and implemented with a singleton pattern so it can be used by every point of interest on any floor.

--- GitHub.CSharpTests/Assets/Scripts/Map/NodeTests.cs
+++ GitHub.CSharpTests/Assets/Scripts/Map/NodeTests.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,22 +13,31 @@ namespace Assets.Scripts
     [TestFixture()]
     public class NodeTests
     {
-        [Test]
-        public void hasAdjacentNodeTest()
-        {
-            Graph g = new Graph();
-            Node n1 = new Node(1, 0, 0, 1);
-            Node n2 = new Node(2, 0, 0, 1);
-            Node n3 = new Node(3, 0, 0, 1);
+        
+            [Test]
+            public void hasAdjacentNodeTest()
+            { 
+                try {
+            
+                    Graph g = new Graph();
+                    Node n1 = new Node(1, 0, 0, 1);
+                    Node n2 = new Node(2, 0, 0, 1);
+                    Node n3 = new Node(3, 0, 0, 1);
 
-            g.InsertNewVertex(n1);
-            g.InsertNewVertex(n2);
-            g.InsertNewVertex(n3);
+                    g.InsertNewVertex(n1);
+                    g.InsertNewVertex(n2);
+                    g.InsertNewVertex(n3);
 
-            n1.addListOfAdjacentNodes(new Dictionary<Node, float>() { { n2, 4 } });
-            n2.addListOfAdjacentNodes(new Dictionary<Node, float>() { { n1, 3 } });
-            Assert.True(n1.hasAdjacentNode(n2),
-                "This tests if hasAdjacentNode returns true if a node is adjacent to a given one.");
-        }
-    }
+                    n1.addListOfAdjacentNodes(new Dictionary<Node, float>() {{n2, 4}});
+                    n2.addListOfAdjacentNodes(new Dictionary<Node, float>() {{n1, 3}});
+                    Assert.True(n1.hasAdjacentNode(n2),
+                        "This tests if hasAdjacentNode returns true if a node is adjacent to a given one.");
+                }
+                catch (SecurityException e)
+                {
+                    Console.WriteLine("Security Exception:\n\n{0}", e.Message);
+                }
+    
+            }
+     }
 }
