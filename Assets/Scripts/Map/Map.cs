34c932ff9dfe42ae74c94e7c0211c4ecf70e5c80
md921 <md921@users.noreply.github.com>
2016-04-09 19:46:02 -0400

    Added displayNode() and conversion of coordinates in Map.cs

--- Assets/Scripts/Map/Map.cs
+++ Assets/Scripts/Map/Map.cs
@@ -15,7 +15,12 @@ namespace Assets.Scripts
         private readonly Graph mapGraph;
         private List<Node> poiList;
         private List<Node> storypointList;
-        private List<FloorPlan> floors; 
+        private List<FloorPlan> floors;
+
+        //Added for dislay
+        public GameObject nodePrefabPOI;
+        public GameObject nodePrefabPOT;
+        public Sprite[] nodeSprites;
 
         public Map()
         {
@@ -97,7 +102,122 @@ namespace Assets.Scripts
             //display the floors of the correct storyline
             storylines[slID].initializeLists(storypointList);
         }
-        
+
+
+        /*
+         * Convert width and height of x-coordinate to match the scale in unity
+         */
+        public float XCoordinatesConversion(float x, float mapWidth)
+        {
+            /*
+                x-> xFromJSON - widthMAPFromJSON/2
+                y-> -(yFromJSON) + heightMAPFromJSON/2
+            */
+
+            // float xMapUnity = 5.76f;
+            // float xMapPixel = 2313f;
+            float xScaled = (2 * 5.76f * mapWidth) / 2313f;
+            float xConverted = x - mapWidth / 2;
+            float xConvertedScaled = (xConverted * (2 * xScaled)) / mapWidth;
+            return xConvertedScaled;
+        }
+
+        /*
+         * Convert width and height of y-coordinate to match the scale in unity
+         */
+        public float YCoordinatesConversion(float y, float mapHeight)
+        {
+            // float yMapUnity = 5.74f;
+            // float yMapPixel = 2328f;
+            float yScaled = (2 * 5.74f * mapHeight) / 2328f;
+            float yConverted = -(y) + mapHeight / 2;
+            float yConvertedScaled = (yConverted * (2 * yScaled)) / mapHeight;
+            return yConvertedScaled;
+        }
+
+        /*
+         * Display poi or pot according to their attributes
+         */
+        public void DisplayNode(List<Node> poiList, FloorPlan floorPlan)
+        {
+
+            int blue = 0;
+            int green = 3;
+
+            Sprite nodeSprite;
+            string nodeColorEditor; // show name of color of the sprite in editor (optional)
+            GameObject newNode;
+
+            foreach (Node n in poiList)
+            {
+
+
+                if (n.GetType() == typeof(PointOfInterest)) //check if poi or pot at runtime type
+                {
+                    newNode = GameObject.Instantiate(nodePrefabPOI) as GameObject; //not exist in this current object must add as
+                    newNode.transform.parent = GameObject.Find("FloorManager").transform;
+                    newNode.SetActive(true);
+
+                    //if id
+
+                    //TODO: need to take specific type of node depending on their type
+                    switch (n.color)
+                    {
+                        case "green":
+                            nodeSprite = nodeSprites[green];
+                            nodeColorEditor = nodeSprite.name; //get sprite color name (optional)
+                            newNode.name = nodeColorEditor; //print color name for specific sprite (optional)
+                            newNode.GetComponent<Node>().setX(XCoordinatesConversion(n.x, floorPlan.getImageWidth()));
+                            newNode.GetComponent<Node>().setY(YCoordinatesConversion(n.y, floorPlan.getImageHeight()));
+                            //                          newNode.GetComponent<Node>().setID(n.getID());
+                            //                          newNode.GetComponent<Node>().setFloorNumber(floor.getFloorNumber());
+                            newNode.GetComponent<SpriteRenderer>().sprite = nodeSprite;
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+                else if (n.GetType() == typeof(PointOfTransition)) //check poi or pot at runtime type
+                {
+                    newNode = GameObject.Instantiate(nodePrefabPOT) as GameObject; //not exist in this current object must add as
+                    newNode.transform.parent = GameObject.Find("FloorManager").transform;
+                    newNode.SetActive(true);
+
+                    switch (n.color)
+                    {
+                        case "blue":
+                            nodeSprite = nodeSprites[blue];
+                            nodeColorEditor = nodeSprite.name; //get sprite color name (optional)
+                            newNode.name = nodeColorEditor; //print color name for specific sprite (optional)
+                            newNode.GetComponent<Node>().setX(XCoordinatesConversion(n.x, floorPlan.getImageWidth()));
+                            newNode.GetComponent<Node>().setY(YCoordinatesConversion(n.y, floorPlan.getImageHeight()));
+                            //                          newNode.GetComponent<Node>().setID(n.getID());
+                            //                          newNode.GetComponent<Node>().setFloorNumber(floor.getFloorNumber());
+                            newNode.GetComponent<SpriteRenderer>().sprite = nodeSprite;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+
+            } //foreach
+        }
+
+        /*
+         * Set camera zoom height and width
+         * Display floorPlan image according to their x-y scaled
+         */
+        public void DisplayMap(FloorPlan floorPlan)
+        {
+            //floorplan.cs
+
+
+
+
+
+        }
 
 
     }
